9e92f6395be22641395ee05528dbd268209f0781
Kajuran <Kajuran65@gmail.com>
2025-09-25 15:01:23 -0400

    more stack problems

---
+++ StacksExpression.java
@@ -0,0 +1,64 @@
+//interview question about the validating arithmetic expression, ensuring there is an opened and closing bracket.
+
+import java.util.Stack;
+
+public class StacksExpression{
+    public static void main(String[] args) {
+        
+
+        String str = "(<>)";
+
+        boolean hello = stackproblem.check(str);
+
+        System.out.println(hello);
+
+
+        
+
+
+
+}
+
+class stackproblem{
+
+    public static boolean check(String str){
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: str.toCharArray()){
+
+            if(ch == '('){
+                stack.push(ch);
+
+            }
+
+            if (ch == '('){
+                stack.pop();
+            }
+
+
+        }
+
+        return stack.empty();
+
+
+    }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
